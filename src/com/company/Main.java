0e43f9c9a57ae411a41f3b42a7de543b13f29c6a
CSS17 <ofucer99@gmail.com>
2021-10-07 15:58:26 +0300

    Yazlab-Proje 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+import processing.core.PApplet;
+import controlP5.*;
+import processing.core.PFont;
+
+public class Main extends PApplet {
+    boolean a=false;
+    boolean b=false;
+    ControlP5 cp5;
+    PFont font;
+    public static void main(String[] args) {
+        PApplet.main("com.company.Main", args);
+
+    }
+    public void settings() {
+        size(600, 600);
+        cp5=new ControlP5(this);
+
+    }
+
+    public void setup() {
+        String[] args = {"TwoFrameTest"};
+        SecondApplet sa = new SecondApplet();
+        PApplet.runSketch(args, sa);
+        surface.setResizable(true);
+        font = createFont("arial.ttf", 24);
+
+    }
+
+    public void draw() {
+        background(0,0,255);
+        surface.setLocation(710, 100);
+        if(b){
+            text("2. Ekrana Tikladin",110,100);
+        }
+    }
+    public void mousePressed(){
+        a=true;
+    }
+
+    //2.Ekran
+    public class SecondApplet extends PApplet {
+        public void setup(){
+            surface.setResizable(true);
+        }
+        public void settings() {
+            size(600, 600);
+
+
+        }
+        public void draw() {
+            background(255,0,0);
+            fill(0);
+            surface.setLocation(100, 100);
+            if(a){
+                text("1. Ekrana Tikladin",100,100);
+            }
+
+
+        }
+        public void mousePressed(){
+            b=true;
+        }
+    }
+
+
+
+
+
+
+}
