4cf4e23dcd7caf98230a77fba9556db4c66c778b
CSS17 <ofucer99@gmail.com>
2021-10-11 18:54:43 +0300

    Başlıyoruz

---
+++ src/Harita.java
@@ -0,0 +1,91 @@
+//package com.company;
+
+import static com.teamdev.jxbrowser.engine.RenderingMode.*;
+
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.view.swing.BrowserView;
+import java.awt.BorderLayout;
+import java.awt.TextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class Harita  {
+
+    private static final int MIN_ZOOM = 0;
+    private static final int MAX_ZOOM = 21;
+    private static final String setMarkerScript =
+            "var myLatlng = new google.maps.LatLng(48.4431727,23.0488126);\n" +
+                    "var marker = new google.maps.Marker({\n" +
+                    "    position: myLatlng,\n" +
+                    "    map: map,\n" +
+                    "    title: 'Hello World!'\n" +
+                    "});";
+
+    /**
+     * In map.html file default zoom value is set to 4.
+     */
+    private static int zoomValue = 4;
+
+    public static void main(String[] args) {
+        Engine engine = Engine.newInstance(
+                EngineOptions.newBuilder(HARDWARE_ACCELERATED)
+                        .licenseKey("1BNDHFSC1G0K0UVU0TQZN5XXZF7V92HYVSLAPKWBCYGQAXGEJUFN4SVZKLDGAX2KA7LAHQ")
+                        .build());
+        System.setProperty("jxbrowser.license.key", "1BNDHFSC1G0K0UVU0TQZN5XXZF7V92HYVSLAPKWBCYGQAXGEJUFN4SVZKLDGAX2KA7LAHQ");
+        Browser browser = engine.newBrowser();
+
+        SwingUtilities.invokeLater(() -> {
+            BrowserView view = BrowserView.newInstance(browser);
+
+            JButton zoomInButton = new JButton("Zoom In");
+            zoomInButton.addActionListener(e -> {
+                if (zoomValue < MAX_ZOOM) {
+                    browser.mainFrame().ifPresent(frame ->
+                            frame.executeJavaScript("map.setZoom(" +
+                                    ++zoomValue + ")"));
+                }
+            });
+
+            JButton zoomOutButton = new JButton("Zoom Out");
+            zoomOutButton.addActionListener(e -> {
+                if (zoomValue > MIN_ZOOM) {
+                    browser.mainFrame().ifPresent(frame ->
+                            frame.executeJavaScript("map.setZoom(" +
+                                    --zoomValue + ")"));
+                    System.out.println("a");
+                }
+            });
+
+            JButton setMarkerButton = new JButton("Set Marker");
+            setMarkerButton.addActionListener(e ->
+                    browser.mainFrame().ifPresent(frame ->
+                            frame.executeJavaScript(setMarkerScript)));
+
+            TextField text1 = new TextField();
+            text1.setSize(100, 100);
+            JPanel toolBar = new JPanel();
+            toolBar.add(zoomInButton);
+            toolBar.add(zoomOutButton);
+            toolBar.add(setMarkerButton);
+            toolBar.add(text1);
+
+
+
+            JFrame frame = new JFrame("Google Maps");
+            frame.add(toolBar, BorderLayout.SOUTH);
+            frame.add(view, BorderLayout.CENTER);
+            frame.setSize(800, 500);
+            frame.setVisible(true);
+
+
+
+
+
+            browser.navigation().loadUrl("C:\\Users\\ofuce\\Desktop\\YAZLAB JS\\yazlab.html");
+        });
+    }
+}
